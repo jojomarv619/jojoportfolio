a9620d8a301f286d0451a1846b38cb9d5782ec6f
Marvin Hipolito <marvinhipolito1600@gmail.com>
2023-01-03 04:26:12 +0800

    third commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {HiArrowNarrowRight} from 'react-icons/hi'
-import Navbar from './Navbar'
+// import Navbar from './Navbar'
 
 const Home = () => {
   return (
@@ -9,11 +9,11 @@ const Home = () => {
             <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
                 <p className='text-pink-600'> Henlo, my name is</p>
                 <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Marvin Joseph Salaber Hipolito</h1>
-                <h2>You can guys can call me jojo</h2> 
-                <h3>I am current a Junior Developer</h3>
+                <h2 className='text-yellow-300'>You can guys can call me jojo</h2> 
+                <h3 className='text-yellow-500'>I am current a Junior Developer</h3>
                 
                 <div>
-                    <button>View 'placeholder'<HiArrowNarrowRight /></button>
+                    <button className='text-yellow-600'>View 'placeholder'<HiArrowNarrowRight /></button>
                 </div>
 
             </div>
