d887304165eea7a4b0d014e7c840ff4e619482f6
Marvin Hipolito <marvin.hipolito@softvision.com>
2023-01-16 17:36:46 +0800

    thriteenth commit

---
+++ src/components/gaming/Games.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+// import css from "assets/professionalSkills/css.png";
+import css from "../../assets/professionalSkills/css.png";
+// import html from "assets/professionalSkills/html.png";
+import html from "../../assets/professionalSkills/html.png";
+// import node from "assets/professionalSkills/node.png";
+import node from "../../assets/professionalSkills/node.png";
+// import react from "assets/professionalSkills/react.png";
+import react from "../../assets/professionalSkills/react.png";
+// import github from "assets/professionalSkills/github.png";
+import github from "../../assets/professionalSkills/github.png";
+
+const Games = () => {
+  return (
+    <div className="portfolio bg-[#0a192f]  text-gray-300 py-4 px-4">
+      {/* Container */}
+      <div className="max-w-[1000px] mt-10 mx-auto p-4 flex flex-col justify-center w-full h-full">
+        <div className="portDescription">
+          <p className="text-4xl font-bold inline border-b-4 border-pink-600">
+            Games
+          </p>
+          <p className="py-4">Games I play</p>
+        </div>
+      </div>
+      <div className="w-full grid grid-cols-2 lg:grid-cols-5 sm:grid-cols-3 gap-4 text-center py-8">
+        <div className="shadow-md shadow-[#00FF00] hover:scale-110 duration-500">
+          <img className="w-20 mx-auto" src={html} alt="html icon" />
+          <p className="my-4">HTML</p>
+        </div>
+        <div className="shadow-md shadow-[#00FF00] hover:scale-110 duration-500">
+          <img className="w-20 mx-auto" src={css} alt="css icon" />
+          <p className="my-4 ">CSS</p>
+        </div>
+        <div className="shadow-md shadow-[#00FF00] hover:scale-110 duration-500">
+          <img className="w-20 mx-auto" src={node} alt="css icon" />
+          <p className="my-4">NODE</p>
+        </div>
+        <div className="reactwidth shadow-md shadow-[#00FF00] hover:scale-110 duration-500">
+          <img className="w-20 mx-auto" src={react} alt="react icon" />
+          <p className="my-4">REACT</p>
+        </div>
+        <div className="shadow-md shadow-[#00FF00] hover:scale-110 duration-500">
+          <img className="w-20 mx-auto" src={github} alt="github icon" />
+          <p className="my-4">GITHUB</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Games;
