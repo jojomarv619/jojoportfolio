4c436342f3c82416b4c93afb12e704752f413e79
Marvin Hipolito <marvinhipolito1600@gmail.com>
2023-01-16 18:27:18 +0800

    Version 1

--- src/App.js
+++ src/App.js
@@ -4,11 +4,10 @@ import Home from "./components/work/Home";
 import Navbar from "./components/work/Navbar";
 import Portfolio from "./components/work/Portfolio";
 import AboutGaming from "./components/gaming/AboutGaming";
-import SkillsGaming from "./components/gaming/SkillsGaming";
+// import SkillsGaming from "./components/gaming/SkillsGaming";
 import HomeGaming from "./components/gaming/HomeGaming";
 import Sidebar from "./components/work/Sidebar";
 import SidebarGaming from "./components/gaming/SidebarGaming";
-
 import "./styles.css";
 import NavbarGaming from "./components/gaming/NavbarGaming";
 import Contact from "./components/work/Contact";
@@ -40,11 +39,9 @@ function App() {
       {gaming && <Portfolio /> }
       {/* {!gaming && <SkillsGaming />} */}
 
-      {gaming && <Contact />}
+      {/* {gaming && <Contact />} */}
 
 
-  
-      
      
     </div>
   );
